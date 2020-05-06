7b13d8aa6a0226781476f2689ba67f4d61956e61
rbtote <a01366943@itesm.mx>
2020-05-06 17:57:27 -0500

    Added back company

--- src/models/empresa.js
+++ src/models/empresa.js
@@ -42,11 +42,11 @@ empresaSchema.virtual('user',{
 
 //Get company by id
 empresaSchema.statics.getCompany = function(id) {
-  if(id.length!=24){
-    return reject('Incorrect id')
-  }
-  const o_id = new ObjectId(id);  
   return new Promise( function(resolve, reject) {
+    if(id.length!=24){
+      return reject('Incorrect id')
+    }
+    const o_id = new ObjectId(id)
     Empresa.findOne({_id:o_id}).then(function(company) {      
       if( !company ) {
         return reject('Company does not exist')
