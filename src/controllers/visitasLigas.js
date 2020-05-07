605b4dfb9757c2a4dba78191ae607de2f4a93d98
rbtote <a01366943@itesm.mx>
2020-05-07 17:47:46 -0500

    consultas ligas

--- src/controllers/visitasLigas.js
+++ src/controllers/visitasLigas.js
@@ -1,7 +1,19 @@
 const Liga = require('../models/ligascortas')
 const VisitaLiga = require('../models/visitaLiga')
 
+const getVisitasLiga = function(req, res) {
+  const liga = req.params.liga
+  console.log(req.params.liga)
+  VisitaLiga.countDocuments({ligaId: liga}).then(function(visitas) {
+    res.send(visitas)
+  }).catch(function(error){
+    res.status(404).send(error)
+  })
+}
+
+
 
   module.exports = {
-    visitLiga : visitLiga
+    visitLiga : visitLiga,
+    getVisitasLiga : getVisitasLiga
   }
